62e122d07020c520b85883ee4a41ae903dcd1306
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-04-25 00:35:59 +0900

    config.phpに切り出し

---
+++ work/app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+/* PDOを使用してアクセス */
+// DSN(データソースネーム): mysql => host名,db名,文字コード
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+// define('SITE_URL', 'http://localhost:8562');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
--- work/public/index.php
+++ work/public/index.php
@@ -1,14 +1,6 @@
 <?php
 
-session_start();
-
-/* PDOを使用してアクセス */
-// DSN(データソースネーム): mysql => host名,db名,文字コード
-define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
-define('DB_USER', 'myappuser');
-define('DB_PASS', 'myapppass');
-// define('SITE_URL', 'http://localhost:8562');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+require_once(__DIR__ . '/../app/config.php'); // 絶対パスで指定する
 
 createToken();
 
